de68e563800b82e2b0a5cf6f9ead81088ce8fa80
Rohini <littlebusy0507@gmail.com>
2026-09-12 10:12:17 +0530

    Add files via upload

---
+++ streq.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Streq {
+    public static void main(String[] args){
+        String s1="Java";
+        String s2="Java";
+        System.out.println(s1==s2);
+        System.out.print(s1.equals(s2));
+    }
+}
